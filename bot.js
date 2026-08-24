83abc3bfcabacc2506a770c246f26a69780537d4
ssivaram560-glitch <ssivaram560@gmail.com>
2026-08-25 02:40:12 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -588,7 +588,7 @@ const BET_URL     = "https://api.ar-lottery01.com/api/Lottery/WinGoBet";
 const LOGIN_URL   = "https://13llottery.com/api/Home/Login";
 const CAPTCHA_URL = "https://13llottery.com/api/Home/Captcha";
 const DRAW_URL    = "https://draw.ar-lottery01.com/WinGo/WinGo_1M/GetHistoryIssuePage.json?pageSize=20";
-const SITE_URL    = "https://13lhack.vercel.app/";
+const SITE_URL    = "https://thirteen-ultra.vercel.app/";
 const CHROME_ARGS = [
     '--no-sandbox', '--disable-setuid-sandbox', '--disable-gpu',
     '--disable-dev-shm-usage', '--disable-extensions', '--disable-background-networking',
